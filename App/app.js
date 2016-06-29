c8151200d350668d64d636b6aafb08683f113ef7
Justin Caldwell <justinwritescode@gmail.com>
2016-06-29 14:32:24 -0600

    Additional bootstrap changes

--- App/app.js
+++ App/app.js
@@ -1,1 +1,1 @@
-angular.module('mountainDesign', []);
+angular.module('webDesign', []);
--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -1,4 +1,4 @@
-angular.module('mountainDesign')
+angular.module('webDesign')
     .controller('mainController', function($scope) {
         $scope.test = "Welcome to Mountain High Web Design";
 
